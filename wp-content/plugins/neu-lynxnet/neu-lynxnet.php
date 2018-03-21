e491c6524087a4aaf226b9d02b9638f6875d130e
Tanner Moushey <tanner@iwitnessdesign.com>
2018-03-21 13:49:15 -0700

    Add support for resource cpt

--- wp-content/plugins/neu-lynxnet/neu-lynxnet.php
+++ wp-content/plugins/neu-lynxnet/neu-lynxnet.php
@@ -40,11 +40,13 @@ class NEU_LynxNet {
 	protected function __construct() {
 		add_filter( 'neu_get_societies', array( $this, 'lynxnet_society' ) );
 		add_action( 'add_user_to_blog', array( $this, 'set_member_type' ) );
+		add_action( 'init', array( $this, 'resources_cpt' ) );
+		add_filter( 'post_link', array( $this, 'resource_link' ), 10, 2 );
 	}
 
 	/**
 	 * @param $societies
-	 * 
+	 *
 	 *
 	 * @since  1.0.0
 	 *
@@ -53,7 +55,7 @@ class NEU_LynxNet {
 	 */
 	public function lynxnet_society( $societies ) {
 		return array(
-			'lynxnet'          => array(
+			'lynxnet' => array(
 				'labels'        => array(
 					'name'          => 'LN',
 					'singular_name' => 'LN',
@@ -67,4 +69,71 @@ class NEU_LynxNet {
 		bp_set_member_type( $user_id, Humanities_Commons::$society_id );
 	}
 
+	/**
+	 * Register resource CPT
+	 *
+	 * @author Tanner Moushey
+	 */
+	public function resources_cpt() {
+
+		$labels = array(
+			'name'               => _x( 'Resources', 'post type general name', 'neu-lynxnet' ),
+			'singular_name'      => _x( 'Resource', 'post type singular name', 'neu-lynxnet' ),
+			'menu_name'          => _x( 'Resources', 'admin menu', 'neu-lynxnet' ),
+			'name_admin_bar'     => _x( 'Resource', 'add new on admin bar', 'neu-lynxnet' ),
+			'add_new'            => _x( 'Add New', 'resource', 'neu-lynxnet' ),
+			'add_new_item'       => __( 'Add New Resource', 'neu-lynxnet' ),
+			'new_item'           => __( 'New Resource', 'neu-lynxnet' ),
+			'edit_item'          => __( 'Edit Resource', 'neu-lynxnet' ),
+			'view_item'          => __( 'View Resource', 'neu-lynxnet' ),
+			'all_items'          => __( 'All Resources', 'neu-lynxnet' ),
+			'search_items'       => __( 'Search Resources', 'neu-lynxnet' ),
+			'parent_item_colon'  => __( 'Parent Resources:', 'neu-lynxnet' ),
+			'not_found'          => __( 'No resources found.', 'neu-lynxnet' ),
+			'not_found_in_trash' => __( 'No resources found in Trash.', 'neu-lynxnet' )
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array(
+				'slug'       => 'resource',
+				'with_front' => false,
+			),
+			'capability_type'    => 'post',
+			'has_archive'        => 'resources',
+			'hierarchical'       => false,
+			'menu_position'      => null,
+			'menu_icon'          => 'dashicons-book-alt',
+			'supports'           => array( 'title', 'editor' )
+		);
+
+		register_post_type( 'resource', $args );
+
+	}
+
+	/**
+	 * Use the feed link as the permalink if applicable
+	 *
+	 * @param $permalink
+	 * @param WP_Post $post
+	 *
+	 * @return mixed
+	 * @author Tanner Moushey
+	 */
+	public function resource_link( $permalink, $post ) {
+		if ( 'resource' !== $post->post_type ) {
+			return $permalink;
+		}
+
+		if ( $feed_link = get_post_meta( $post->ID, 'feed_link', true ) ) {
+			return $feed_link;
+		}
+
+		return $permalink;
+	}
 }
